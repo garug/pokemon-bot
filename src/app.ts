fc448145bac576d2ae0e874c77d556eb8986b979
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-10-24 23:38:40 -0300

    Tempo incorreto

--- src/app.ts
+++ src/app.ts
@@ -240,7 +240,7 @@ app.post("/@me", async (req, res) => {
   return res.json(usuario.data);
 });
 
-const maxInterval = 4 * 60 * 1000;
+const maxInterval = 12 * 60 * 1000;
 
 const chanceInterval = process.env.DEVMODE
   ? 24 * 60 * 60 * 1000
