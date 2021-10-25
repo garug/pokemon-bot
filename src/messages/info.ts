8c00c53ad62bc922c75324e478dc4663ec71a535
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-10-24 21:46:16 -0300

    Update de versão discord

--- src/messages/info.ts
+++ src/messages/info.ts
@@ -2,42 +2,42 @@ import { Message, MessageEmbed } from "discord.js";
 import MoreStrongPokemon from "../models/MoreStrongPokemon";
 
 export default async function info(m: Message) {
-    const infoAbout = m.content.toLowerCase().split(" ");
-    const ownedPokemon = await MoreStrongPokemon.aggregate([
-        {
-          $addFields: {
-            total: {
-              $add: [
-                "$attributes.attack",
-                "$attributes.defense",
-                "$attributes.hp",
-                "$attributes.sp_attack",
-                "$attributes.sp_defense",
-                "$attributes.speed",
-              ],
-            },
-          },
+  const infoAbout = m.content.toLowerCase().split(" ");
+  const ownedPokemon = await MoreStrongPokemon.aggregate([
+    {
+      $addFields: {
+        total: {
+          $add: [
+            "$attributes.attack",
+            "$attributes.defense",
+            "$attributes.hp",
+            "$attributes.sp_attack",
+            "$attributes.sp_defense",
+            "$attributes.speed",
+          ],
         },
-        {
-          $sort: {
-            total: -1,
-          },
-        },
-        {
-          $match: {
-            user: m.author.id,
-          },
-        },
-      ]);
-      const uniquePokemon = [...new Set(ownedPokemon.map((p) => p.name))];
-      const strongest = [...ownedPokemon].splice(0, 6).map((e) => e.name);
-      const reply = new MessageEmbed().setColor("#f39c12").setDescription(
-        `${m.author} dex: ${uniquePokemon.length}/493
+      },
+    },
+    {
+      $sort: {
+        total: -1,
+      },
+    },
+    {
+      $match: {
+        user: m.author.id,
+      },
+    },
+  ]);
+  const uniquePokemon = [...new Set(ownedPokemon.map((p) => p.name))];
+  const strongest = [...ownedPokemon].splice(0, 6).map((e) => e.name);
+  const reply = new MessageEmbed().setColor("#f39c12").setDescription(
+    `${m.author} dex: ${uniquePokemon.length}/493
     
         Strongest pokemon: ${strongest}
         
         Full dex: ${process.env.FRONTEND_URL}/usuarios/${m.author.id}`
-      );
-      // [Full dex](https://vigilant-villani-fedc91.netlify.app/#/usuarios/${m.author.id})
-      m.channel.send(reply);
+  );
+  // [Full dex](https://vigilant-villani-fedc91.netlify.app/#/usuarios/${m.author.id})
+  m.channel.send({ embeds: [reply] });
 }
