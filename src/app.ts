ff0a0c3c0f2d08074a082ec51d86e3843424e1ae
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-11-21 16:14:29 -0300

    Spawn aumentado por hje

--- src/app.ts
+++ src/app.ts
@@ -244,7 +244,7 @@ app.post("/@me", async (req, res) => {
   return res.json(usuario.data);
 });
 
-const maxInterval = 12 * 60 * 1000;
+const maxInterval = 3 * 60 * 1000;
 
 const chanceInterval = process.env.DEVMODE
   ? 24 * 60 * 60 * 1000
