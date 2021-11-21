7c15c7d4f0b7f1ce299435de009e56aa52b856af
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-11-21 11:04:39 -0300

    Versão inicial de tier de pokemon

--- src/managers/tier.ts
+++ src/managers/tier.ts
@@ -1,8 +1,13 @@
+import { head } from "lodash";
+import { ConnectableObservable } from "rxjs";
+import InfoPokemon from "../models/InfoPokemon";
 import OwnedPokemon from "../models/OwnedPokemon";
 import Prestige from "../models/Prestige";
+import MoreStrongPokemon from "../models/views/MoreStrongPokemon";
 
 export interface Tier {
   name: TierName;
+  value: number;
   when: (value: number) => boolean;
   mod_pokemon: number;
   mod_trainer: number;
@@ -13,57 +18,65 @@ export type TierName = "SS" | "S" | "A" | "B" | "C" | "D" | "E" | "F";
 export const availableTiers: Tier[] = [
   {
     name: "SS",
+    value: 0.01,
     when: (value) => value > 0 && value <= 0.01,
     mod_pokemon: 0.05,
     mod_trainer: 5,
   },
   {
     name: "S",
+    value: 0.02,
     when: (value) => value > 0.01 && value <= 0.03,
     mod_pokemon: 0.1,
     mod_trainer: 3,
   },
   {
     name: "A",
+    value: 0.07,
     when: (value) => value > 0.03 && value <= 0.1,
     mod_pokemon: 0.25,
     mod_trainer: 2,
   },
   {
     name: "B",
+    value: 0.1,
     when: (value) => value > 0.1 && value <= 0.2,
     mod_pokemon: 0.5,
     mod_trainer: 1.5,
   },
   {
     name: "C",
+    value: 0.2,
     when: (value) => value > 0.2 && value <= 0.4,
     mod_pokemon: 0.8,
     mod_trainer: 1.25,
   },
   {
     name: "D",
+    value: 0.25,
     when: (value) => value > 0.4 && value <= 0.65,
     mod_pokemon: 1,
     mod_trainer: 1,
   },
   {
     name: "E",
+    value: 0.225,
     when: (value) => value > 0.65 && value <= 0.875,
     mod_pokemon: 1.25,
     mod_trainer: 0.8,
   },
   {
     name: "F",
+    value: 0.125,
     when: (value) => value > 0.875,
     mod_pokemon: 1.5,
     mod_trainer: 0.5,
   },
 ];
 
-const fnList = (list: any[], pList: any[]) => {
+export const fnList = (list: any[], pList: any[]) => {
   if (list.length < pList.length) {
-    return pList.map((_, i) => list[i]);
+    return pList.filter((_, i) => list[i]).map((_, i) => [list[i]]);
   }
 
   const state = {
@@ -127,4 +140,51 @@ export async function updateTrainers() {
   //   const savedOf = saved.filter((s) => s.pokemon === info.number);
   //   console.log(savedOf);
   // });
+}
+
+export async function updatePokemon() {
+  const allPoke = await MoreStrongPokemon.aggregate([
+    {
+      $group: {
+        _id: "$number",
+        name: {
+          $first: "$name",
+        },
+        arr: {
+          $push: {
+            total: "$total",
+          },
+        },
+      },
+    },
+  ]);
+
+  const updates = allPoke.map((p) => {
+    const fn = fnList(
+      p.arr,
+      availableTiers.map((t) => t.value)
+    );
+
+    const tiers = availableTiers
+      .filter((_, index) => fn[index])
+      .map((t, index) => ({
+        order: index,
+        tier: t.name,
+        value: fn[index][fn[index].length - 1].total,
+      }));
+
+    return InfoPokemon.updateOne(
+      {
+        number: p._id,
+      },
+      {
+        $set: {
+          tiers,
+        },
+      },
+      { upsert: true }
+    );
+  });
+
+  await Promise.all(updates);
 }
--- src/models/InfoPokemon.ts
+++ src/models/InfoPokemon.ts
@@ -3,11 +3,23 @@ import { Document, model, Schema } from "mongoose";
 export interface InfoPokemon extends Document {
   number: number;
   name: string;
+  tiers: {
+    order: number;
+    tier: string;
+    value: number;
+  }[];
 }
 
 export const InfoPokemonSchema = new Schema({
   number: Number,
   name: String,
+  tiers: [
+    {
+      order: Number,
+      tier: String,
+      value: Number,
+    },
+  ],
 });
 
 export default model<InfoPokemon>("InfoPokemon", InfoPokemonSchema);
