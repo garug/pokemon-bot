100e009b18dad1c9432e782377a9384914e1c1d4
Allef Frazão Santana (Garug) <allefff@gmail.com>
2022-07-17 22:24:49 -0300

    Fix b8bb6802

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,43 +1,31 @@
-import { generate } from "randomstring";
-import Pokemon from "../Pokemon";
-import { moves } from "./moves";
-
-export function randomFromInterval(min: number, max: number) {
-  return (
-    Math.floor(Math.random() * (max * 100 - min * 100 + 1) + min * 100) / 100
-  );
-}
-
-export function randomPokemon() {
-  return new Pokemon({
-    id: generate(6),
-    name: `abc${randomFromInterval(1, 1000)}`,
-    number: 1,
-    level: 1,
-    attributes: {
-      hp: 50,
-      attack: 50,
-      defense: 50,
-      sp_attack: 50,
-      sp_defense: 50,
-      speed: 50,
-    },
-    moves,
-    types: ["normal"],
-  });
-}
+import { random } from "lodash";
 
 export function generateNumber(number: number) {
-  const chance = Math.random();
-  if (chance < 0.7) {
-    return number * randomFromInterval(0.8, 1.25);
-  } else if (chance < 0.9) {
-    return number * randomFromInterval(1.25, 1.4);
-  } else if (chance < 0.95) {
-    return number * randomFromInterval(1.4, 1.65);
-  } else if (chance < 0.99) {
-    return number * randomFromInterval(1.75, 2);
-  } else {
-    return number * 3;
-  }
+    const chance = Math.random();
+    if (chance < 0.7) {
+        return number * random(0.8, 1.25);
+    } else if (chance < 0.9) {
+        return number * random(1.25, 1.4);
+    } else if (chance < 0.95) {
+        return number * random(1.4, 1.65);
+    } else if (chance < 0.99) {
+        return number * random(1.75, 2);
+    } else {
+        return number * 3;
+    }
+}
+
+export function sort<Type>(possibities: Type[], mapToPossibility: (param: Type) => number = () => 1): Type {
+    let total = 0;
+
+    const usedPossibilities = possibities
+        .map(mapToPossibility)
+        .map(n => {
+            total += n;
+            return total;
+        });
+
+    const index = usedPossibilities.findIndex((n) => n >= random(1, total));
+
+    return possibities[index];
 }
