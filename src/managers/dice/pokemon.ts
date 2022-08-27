39932764a2df9395287613df5f93729162b9f3a2
Allef Santana <allefff@gmail.com>
2022-08-27 10:28:51 -0300

    Unused files

--- src/managers/dice/pokemon.ts
+++
@@ -1,9 +0,0 @@
-import { Attributes } from "@models/OwnedPokemon";
-import { Move } from "./moves";
-
-export interface Pokemon {
-    moves: Moves,
-    attributes: Attributes,
-}
-
-export type Moves = [Move, Move, Move, Move];
